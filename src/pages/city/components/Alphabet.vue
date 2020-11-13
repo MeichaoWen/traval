54bffa95a48dbe91b2c5d55064cd76cd79b7a761
wenmeichao <wenmeichao@bjshenpu.com>
2020-11-13 22:22:35 +0800

    xingneng

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -21,8 +21,13 @@ export default {
   },
   data () {
     return {
-      touchStatus: false
+      touchStatus: false,
+      startY: 0,
+      timer: null
     }
+  },
+  updated () {
+    this.startY = this.$refs['A'][0].offsetTop
   },
   computed: {
     letter () {
@@ -42,12 +47,16 @@ export default {
     },
     handleTouchmove (e) {
       if (this.touchStatus) {
-        const startY = this.$refs['A'][0].offsetTop
-        const touchY = e.touches[0].clientY - 79
-        const index = Math.floor((touchY - startY) / 20)
-        if (index >= 0 && index < this.letter.length) {
-          this.$emit('change', this.letter[index])
+        if (this.timer) {
+          clearTimeout(this.timer)
         }
+        this.timer = setTimeout(() => {
+          const touchY = e.touches[0].clientY - 79
+          const index = Math.floor((touchY - this.startY) / 20)
+          if (index >= 0 && index < this.letter.length) {
+            this.$emit('change', this.letter[index])
+          }
+        }, 16)
       }
     },
     handleTouchend () {
